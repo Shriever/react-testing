7fc47d85d4240f05eeef9200038c8a6229e8103a
Levi <shrieverdev@gmail.com>
2021-05-03 16:23:48 -0700

    added clear and submit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Enzyme, { shallow, mount } from 'enzyme';
-import App from './App';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import App from "./App";
+import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('App', () => {
+describe("App", () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(<App />);
@@ -14,33 +14,67 @@ describe('App', () => {
     expect(wrapper.contains(<th>Items</th>)).toBe(true);
   });
 
-  it('should have a `button` element', () => {
+  it("should have a `button` element", () => {
     expect(wrapper.containsMatchingElement(<button>Add Item</button>)).toBe(
       true
     );
   });
 
-  it('should have an `input` element', () => {
+  it("should have an `input` element", () => {
     expect(wrapper.containsMatchingElement(<input />)).toBe(true);
   });
-  describe('the user populates the input', () => {
-    const item = 'Best text';
+  describe("the user populates the input", () => {
+    const item = "Best text";
     beforeEach(() => {
-      const input = wrapper.find('input').first();
-      input.simulate('change', {
+      const input = wrapper.find("input").first();
+      input.simulate("change", {
         target: { value: item },
       });
     });
-    it('should change input value', () => {
-      const input = wrapper.find('input').first();
+    it("should change input value", () => {
+      const input = wrapper.find("input").first();
       expect(input.props().value).toEqual(item);
     });
-    it('should enable `button`', () => {
-      const button = wrapper.find('button', () => {
-        expect(button.props().disabled).toBe(false);
+    it("should enable `button`", () => {
+      const button = wrapper.find("button").first();
+      expect(button.props().disabled).toBe(false);
+    });
+    describe("and then clears the input", () => {
+      beforeEach(() => {
+        const input = wrapper.find("input").first();
+        input.simulate("change", {
+          target: { value: "" },
+        });
       });
-      describe('and then clears the input', () => {});
-      describe('and then submits', () => {});
+      it("should clear `input` value", () => {
+        const input = wrapper.find("input").first();
+        expect(input.props().value).toEqual("");
+      });
+      it("should disable the `button`", () => {
+        const button = wrapper.find("button").first();
+        expect(button.props().disabled).toBe(true);
+      });
+    });
+    describe("and then submits", () => {
+      beforeEach(() => {
+        const form = wrapper.find("form").first();
+        form.simulate("submit", { preventDefault: () => {} });
+      });
+      it("should clear `input` value", () => {
+        const input = wrapper.find("input").first();
+        expect(input.props().value).toEqual("");
+      });
+      it("should disable the `button`", () => {
+        const button = wrapper.find("button").first();
+        expect(button.props().disabled).toBe(true);
+      });
+      it("should create a new `td` element", () => {
+        expect(wrapper.containsMatchingElement(<td>{item}</td>)).toBe(true)
+      });
+      it("`td` should have the same value that was submitted", () => {
+        const td = wrapper.find("td").first();
+        expect(td.props().children).toEqual(item);
+      });
     });
   });
 });
